f2339de3be95e1a409cccfcedd024b645facd7fd
victorvs0uz4 <cardoso.vitor516@gmail.com>
2019-06-13 12:02:13 -0300

    .

--- MySQL/primeiros-comandos.sql
+++ MySQL/primeiros-comandos.sql
@@ -15,17 +15,17 @@ DESC CLIENTE;
 
 /* Formas de adicionar dados em uma tabela */
 /* 01 - Excluindo as colunas (os dados devem estar na ordem correta, de acordo com a tabela): */
-INSERT INTO CLIENTE VALUES('Victor','M','victor@gmail.com',23456515,'971185030','RUA JACARÉ','Atibaia','SP');
-INSERT INTO CLIENTE VALUES('Paulo','M','paulo@gmail.com',51151318,'4414218','RUA PEDRO','Pinhalzinho','SP');
-INSERT INTO CLIENTE VALUES('Magda','F','magda@gmail.com',54515184,'8745411','RUA PEDRO SANCHEZ','Bragança Paulista','SP');
-INSERT INTO CLIENTE VALUES('Carla','F','carla@gmail.com',98451178,'2411658','RUA DO AMARAL','Extrema','MG');
+INSERT INTO CLIENTE VALUES('Victor','M','victor@gmail.com',23456515,'971185030','Rua Jacaré - Caetetuba','Atibaia','SP');
+INSERT INTO CLIENTE VALUES('Paulo','M','paulo@gmail.com',51151318,'4414218','Rua Augusta - Centro','Pinhalzinho','SP');
+INSERT INTO CLIENTE VALUES('Magda','F','magda@gmail.com',54515184,'8745411','Rua Pedro Sanchez - Caetetuba','Bragança Paulista','SP');
+INSERT INTO CLIENTE VALUES('Carla','F','carla@gmail.com',98451178,'2411658','Rua do Amaral - Centro','Extrema','MG');
 
 /* 02 - Colocando as colunas, podemos omitir algumas informações, sem passar o parâmetro NULL: */
-INSERT INTO CLIENTE(NOME,SEXO,CPF,TELEFONE,ENDERECO,CIDADE,ESTADO) VALUES('Gabriela','F',54151181,'40028922','RUA PAULISTA','Socorro','SP');
+INSERT INTO CLIENTE(NOME,SEXO,CPF,TELEFONE,ENDERECO,CIDADE,ESTADO) VALUES('Gabriela','F',54151181,'40028922','Rua Paulista - Imperial','Socorro','SP');
 
 /* 03 - Insert compacto, inserir vários registros com apenas um único comando - somente MySQL */
-INSERT INTO CLIENTE VALUES('José','F','jose@gmail.com',8845469,'2928416','RUA PIRITUBA','Socorro','SP'),
-                          ('Pintos Valdo','F','pvaldo@gmail.com',44121554,'97462318','RUA SÃO JOSÉ','Socorro','SP');
+INSERT INTO CLIENTE VALUES('José','M','jose@gmail.com',8845469,'2928416','Rua Pirituba - Ressaca','Socorro','SP'),
+                          ('Pintos Valdo','M','pvaldo@gmail.com',44121554,'97462318','Rua Igarata - Centro','Socorro','SP');
 
 ------------------------------------------------------------------------------------------------------------------
 
@@ -34,7 +34,7 @@ INSERT INTO CLIENTE VALUES('José','F','jose@gmail.com',8845469,'2928416','RUA P
 /* 01 - Mostrando o AGORA */
 SELECT NOW();
 
-/* 02 - ALIAS de Coluna */
+/* 02 - ALIAS de Coluna, criando colunas que não existem nas tabelas */
 SELECT 'VICTOR' AS 'NETWORK ADMDIN';
 
 /* 03 - SELECT filtrando conteúdo especifico */
@@ -42,3 +42,13 @@ SELECT NOME, TELEFONE, CIDADE FROM CLIENTE;
 
 /* 04 - Apenas para fins educativo, o '*' filtra TUDO! */
 SELECT * FROM CLIENTE;
+
+/* Filtrando dados com WHERE, passando as colunas */
+SELECT NOME, SEXO FROM CLIENTE WHERE SEXO = 'M';
+
+/* Trazendo apenas cadastros do sexo masculino sem passar a coluna 'SEXO') */
+SELECT NOME FROM CLIENTE WHERE SEXO = 'M';
+
+/* Utilizando o LIKE (Me traga o resultado que se pareça ou seja como)*/
+/* Caracter coringa: %, tudo que começa ou tudo que termina */
+SELECT NOME, SEXO FROM CLIENTE WHERE ESTADO LIKE '%SP';
